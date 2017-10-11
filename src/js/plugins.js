9c0e68fd4be691ced94cb25b29045e9d599774ad
SimonLoir <simon-loir@hotmail.com>
2017-10-11 16:58:28 +0200

    adding restore scode function in command palette

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -133,5 +133,28 @@ var commands = [
     }],
     ['Show help', 'help', function() {
         newTab('src/res/readme.md', true);
+    }],
+    ["Restore SCode", "scode-restore", function (){
+        
+            var xfs = require("fs");
+            rmdir(os.homedir() + "/.scode/");
+            alert("--- 100% ---")
+            var window = app.getCurrentWindow();
+            window.reload();
+
     }]
 ]
+
+function rmdir(dir_path) {
+    if (fs.existsSync(dir_path)) {
+        fs.readdirSync(dir_path).forEach(function(entry) {
+            var entry_path = path.join(dir_path, entry);
+            if (fs.lstatSync(entry_path).isDirectory()) {
+                rimraf(entry_path);
+            } else {
+                fs.unlinkSync(entry_path);
+            }
+        });
+        fs.rmdirSync(dir_path);
+    }
+}

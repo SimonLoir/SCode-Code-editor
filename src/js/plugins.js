2dfc253832c7e3e003bbb4e4f380662c6310fb9c
SimonLoir <simon-loir@hotmail.com>
2017-10-29 14:39:42 +0100

    updating terminal

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -133,6 +133,9 @@ var commands = [
     ['Show help', 'help', function() {
         newTab('src/res/readme.md', true);
     }],
+    ['New Terminal', 'scode-new-terminal', function() {
+        newTerminal();
+    }],
     ["Restore SCode", "scode-restore", function (){
         
             var xfs = require("fs");
@@ -205,3 +208,92 @@ $(document).ready(function () {
         
     }
 });
+
+function newTerminal(){
+    var x_term = $(document.body).child("div");
+    x_term.css('z-index', 100);
+    x_term.css('position', "fixed");
+    x_term.css('overflow', "hidden");
+    x_term.css('bottom', "25px");
+    x_term.css('height', "250px");
+    x_term.css('max-height', "90%");
+    x_term.css('left', "0");
+    x_term.css('right', "0");
+    x_term.css('background', "rgb(34,34,34)");
+    x_term.css('padding', "5px");
+    var x_x_term = x_term;
+    var x_term_resizer = x_term.child("div");
+
+    var m_pos;
+    function resize(e) {
+        var parent = resize_el.parentNode;
+        var dx = m_pos - e.y;
+        m_pos = e.y;
+        parent.style.height = (parseInt(getComputedStyle(parent, '').height) + dx) + "px";
+        term.fit();
+    }
+
+    var resize_el = x_term_resizer.get(0);
+    resize_el.addEventListener("mousedown", function (e) {
+        m_pos = e.y;
+        document.addEventListener("mousemove", resize, false);
+    }, false);
+    document.addEventListener("mouseup", function () {
+        document.removeEventListener("mousemove", resize, false);
+    }, false);
+
+    x_term_resizer.css('position', "absolute");
+    x_term_resizer.css('top', "0px");
+    x_term_resizer.css('left', "0");
+    x_term_resizer.css('right', "0");
+    x_term_resizer.css('background', "#ccc");
+    x_term_resizer.css('height', "4px");
+    x_term_resizer.css('cursor', "n-resize");
+
+    var term = new Terminal();
+    term.open(x_term.get(0));
+    term.cursorBlink = true
+    term.fit()
+    var pty = require('node-pty');
+
+    var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+    var ptyProcess = pty.spawn(shell, [], {
+        name: 'xterm-color',
+        cwd: folder[0],
+        env: process.env
+    });
+
+    ptyProcess.on('data', function (data) {
+        term.write(data)
+    });
+
+    ptyProcess.on('close', function (data) {
+        x_term.remove();
+    });
+
+    ptyProcess.on('exit', function (data) {
+        x_term.remove();
+    });
+
+    term.textarea.onkeydown = function (e) {
+        if (e.key == "Backspace") {
+            ptyProcess.write("\b");
+        } else if (e.key == "Enter") {
+            ptyProcess.write("\r");
+        } else if (e.key == "c" && e.ctrlKey == true) {
+            ptyProcess.write("\x03");
+        } else {
+            ptyProcess.write(e.key);
+        }
+
+    }
+
+    term.attachCustomKeyEventHandler(function (e) {
+        if (e.keyCode == 9) {
+            // Do nothing
+            return false;
+        }
+    });
+
+}

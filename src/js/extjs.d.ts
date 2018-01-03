cad06cf54a2dbba6e3fd113697b3c89469eff21a
SimonLoir <simon-loir@hotmail.com>
2018-01-03 15:29:42 +0100

    added doc for extjs.click and extjs.ready

--- src/js/extjs.d.ts
+++ src/js/extjs.d.ts
@@ -1,5 +1,15 @@
 interface ExtJsObject{
-
+    /**
+     * Calls the callback when the document is fully loaded.
+     * @param callback Function that is called when the document is ready.
+     */
+    ready(callback: () => void)
+    /**
+     * It calls the callback when the user performs a click on a sepcific element
+     * @param callback Function called when the event is triggered
+     * @param element Query selector of the element
+     */
+    click(callback: (event:Event) => void, element?: String)
 }
 declare module "extjs"{
     /**

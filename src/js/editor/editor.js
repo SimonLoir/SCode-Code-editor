0f91150d14b2fb26d6abf25aacbb870008c4bfbd
SimonLoir <simon-loir@hotmail.com>
2018-01-03 15:00:04 +0100

    updating gitignore

--- src/js/editor/editor.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Editor {
-    constructor() {
-    }
-}
-exports.default = Editor;
-//# sourceMappingURL=editor.js.map
--- src/js/tools/build_tool.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=build_tool.js.map
